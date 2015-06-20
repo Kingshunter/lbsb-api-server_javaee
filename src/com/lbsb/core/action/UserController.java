7f8529ed2c4ca1b3d571bdc9805c0feb18287938
Kingshunter <babysai1988@vip.qq.com>
2015-06-20 16:30:49 +0800

    modify

--- src/com/lbsb/core/action/UserController.java
+++ src/com/lbsb/core/action/UserController.java
@@ -1,65 +1,17 @@
 package com.lbsb.core.action;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.lbsb.base.cache.EhcacheOperation;
-import com.lbsb.base.cache.RemoteCacheManager;
-import com.lbsb.core.model.User;
-import com.lbsb.core.service.UserService;
 
 @Controller
 @RequestMapping("/find")
 public class UserController {
 	
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private RemoteCacheManager mo;
-	
-	@Autowired
-	private EhcacheOperation c;
-	
 	
 	@RequestMapping(value="/user", method = RequestMethod.GET)
 	public void find() {
-		/*System.out.println("11111");
-		User user = userService.getUserById(1);
-		System.out.println(user);*/
-		System.out.println(mo.toString());
-		mo.set("aaa", 30, "afafd");
-		System.out.println(mo.add("bbb", 30, "112"));
-		System.out.println(mo.add("bbb", 30, "112"));
-		List<User> list = new ArrayList<User>();
-		User user1 = new User();
-		user1.setAddress("aaaadfasdf");
-		user1.setId(1);
-		user1.setName("abc");
-		User user2 = new User();
-		user2.setAddress("yuiyuiy");
-		user2.setId(2);
-		user2.setName("errr");
-		list.add(user1);
-		list.add(user2);
-		mo.set("model", user1);
-		mo.set("list", list);
-		Object value = mo.get("aaa");
-		user1 = (User) mo.get("model");
-		list = (List<User>) mo.get("list");
-		System.out.println(value);
-		System.out.println(user1);
-		System.out.println(list);
 		
-		c.set("bbbaaaa", "ewqroipiropqr", "10s");
-		System.out.println(c.get("bbbaaaa"));
-		System.out.println(c.get("bbbaaaa"));
-		System.out.println(c.get("bbbaaaa"));
 	}
 	
 	
